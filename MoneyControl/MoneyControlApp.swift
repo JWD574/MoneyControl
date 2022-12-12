c571f97756970a5cd37dc1ae1ec68102405b1d3a
rangerboy574 <jdeweese574@gmail.com>
2022-12-12 10:32:40 -0600

    Initial Commit

---
+++ MoneyControl/MoneyControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyControlApp.swift
+//  MoneyControl
+//
+//  Created by Joseph  DeWeese on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
